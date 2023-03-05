378ab0b18e58491b73aae5f33e9f87b52938f531
Sohel <absohel47@gmail.com>
2023-03-05 01:26:44 +0000

    two APIs with two thunk functions

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const store = require("./rtk/app/store");
-const { fetchVideos } = require("./rtk/features/video/videoSlice");
+const {
+  fetchVideoTags,
+  fetchVideoswithTags,
+} = require("./rtk/features/video/videoSlice");
 
 store.subscribe(() => {
   // console.log(store.getState());
 });
 
-store.dispatch(fetchVideos());
+store
+  .dispatch(fetchVideoTags())
+  .then(() => {
+    const tags = store.getState().video.tags;
+    // console.log("Tags:", tags);
+    return store.dispatch(fetchVideoswithTags(tags));
+  })
+  .catch((error) => {
+    console.error("Error fetching tags and videos:", error.message);
+  });
--- rtk/features/video/videoSlice.js
+++ rtk/features/video/videoSlice.js
@@ -3,58 +3,80 @@ const axios = require("axios");
 
 // initial state
 const initialState = {
-  videos: [],
+  tags: [],
+  videosWithTag: [],
   error: "",
 };
 
 // create async thunk with axios
-const fetchVideos = createAsyncThunk("video/fetchVideos", async () => {
-  const response1 = await axios.get("http://localhost:9000/videos");
+const fetchVideoTags = createAsyncThunk("video/fetchVideos", async () => {
+  try {
+    const response1 = await axios.get("http://localhost:9000/videos");
 
-  const video = response1.data;
-  const tags = video.tags.map((tag) => tag);
-  const tagParams = tags.map((tag) => `tags_like=${tag}`).join("&");
-  // console.log(tagParams);
+    const video = response1.data;
+    const tags = video.tags.map((tag) => tag);
 
-  const response2 = await axios.get(
-    `http://localhost:9000/videos?${tagParams}`
-  );
-
-  const videos = response2.data;
-
-  // sort videos in descending order by view count
-  videos.sort((a, b) => {
-    const aViews = Number(a.views.replace(/[^\d.-]/g, ""));
-    const bViews = Number(b.views.replace(/[^\d.-]/g, ""));
-    return bViews - aViews;
-  });
-
-  // videos.forEach((video) => {
-  //   console.log(video.views);
-  // });
-
-  // console.log(`http://localhost:9000/videos?${tagParams}`);
-  // console.log(tags);
-
-  return videos;
+    return tags;
+  } catch (error) {
+    console.error("Error fetching videos:", error.message);
+    throw error;
+  }
 });
+
+const fetchVideoswithTags = createAsyncThunk(
+  "video/fetchVideoswithTags",
+  async (tags) => {
+    try {
+      const tagParams = tags.map((tag) => `tags_like=${tag}`).join("&");
+      // console.log(tagParams);
+
+      const response2 = await axios.get(
+        `http://localhost:9000/videos?${tagParams}`
+      );
+
+      const videos = response2.data;
+
+      // sort videos in descending order by view count
+      videos.sort((a, b) => {
+        const aViews = Number(a.views.replace(/[^\d.-]/g, ""));
+        const bViews = Number(b.views.replace(/[^\d.-]/g, ""));
+        return bViews - aViews;
+      });
+
+      // videos.forEach((video) => {
+      //   console.log(video.views);
+      // });
+
+      // console.log(`http://localhost:9000/videos?${tagParams}`);
+      // console.log(tags);
+
+      return videos;
+    } catch (error) {
+      console.error("Error fetching videos:", error.message);
+      throw error;
+    }
+  }
+);
 
 const videoSlice = createSlice({
   name: "video",
   initialState,
   extraReducers: (builder) => {
-    builder.addCase(fetchVideos.pending, (state, action) => {
-      state.error = "";
+    builder.addCase(fetchVideoTags.fulfilled, (state, action) => {
+      state.tags = action.payload;
     });
-
-    builder.addCase(fetchVideos.fulfilled, (state, action) => {
-      state.videos = action.payload;
+    builder.addCase(fetchVideoTags.rejected, (state, action) => {
+      state.error = action.error.message;
     });
-    builder.addCase(fetchVideos.rejected, (state, action) => {
+    builder.addCase(fetchVideoswithTags.fulfilled, (state, action) => {
+      state.videosWithTag = action.payload;
+    });
+    builder.addCase(fetchVideoswithTags.rejected, (state, action) => {
       state.error = action.error.message;
     });
   },
 });
 
 module.exports = videoSlice.reducer;
-module.exports.fetchVideos = fetchVideos;
+module.exports.fetchVideoTags = fetchVideoTags;
+module.exports.fetchVideoswithTags = fetchVideoswithTags;
